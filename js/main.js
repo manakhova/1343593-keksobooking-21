43e63ce1488d4d0cd8e6b777388209befad2d263
Tatiana Manakhova <tat.manakhova@gmail.com>
2020-10-13 14:54:47 +0300

    Выводит объявления на карту

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@
 
 const map = document.querySelector('.map');
 const mapPins = map.querySelector('.map__pins');
-// const cardTemplate = document.querySelector('#card').content.querySelector('.map__card');
+const mapFilter = map.querySelector('.map__filters-container');
+const cardTemplate = document.querySelector('#card').content.querySelector('.map__card');
 const pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 const type = {
   palace: 'Дворец',
@@ -19,35 +20,32 @@ const PIN_HEIGHT = 70;
 const MAP_WIDTH = 1200;
 const MAP_HEIGHT_TOP = 130;
 const MAP_HEIGHT_BOTTOM = 630;
-const someFeachure = getRandomInteger(0, allFeatures.length);
-const somePhoto = getRandomInteger(0, allPhotos.length);
 
 
-// Рандомное число в диапазоне
 function getRandomInteger(min, max) {
   return Math.floor(Math.random() * (max + 1 - min) + min);
 }
 
-// Рандомное свойство объекта
+
 function getRandomProperty(obj) {
   const keys = Object.keys(obj);
   return obj[keys[getRandomInteger(0, keys.length)]];
 }
 
-// Рандомное число удобств
+
 function getRandomFeatures(quantity) {
   const features = [];
   for (let i = 0; i < quantity; i++) {
-    features.push(features[getRandomInteger(0, allFeatures.length)]);
+    features.push(allFeatures[getRandomInteger(0, allFeatures.length - 1)]);
   }
   return features;
 }
 
-// Рандомные фотографии
+
 function getRandomPhotos(quantity) {
   const photos = [];
   for (let i = 0; i < quantity; i++) {
-    photos.push(photos[getRandomInteger(0, allPhotos.length)]);
+    photos.push(allPhotos[getRandomInteger(0, allPhotos.length - 1)]);
   }
   return photos;
 }
@@ -58,20 +56,20 @@ function generateOffersArray(quantity) {
   for (let i = 0; i < quantity; i++) {
     offers.push({
       author: {
-        avatar: `img/avatars/user0${getRandomInteger(1, 8)}.png` // не соображу, как заставить числа не повторяться
+        avatar: `img/avatars/user0${getRandomInteger(1, 8)}.png`
       },
       offer: {
         title: 'Предложение',
         address: `${getRandomInteger(0, 1200) - PIN_WIDTH / 2}, ${getRandomInteger(130, 630) - PIN_HEIGHT}`,
-        price: getRandomInteger(1, 10000),
+        price: `${getRandomInteger(1, 10000)}₽/ночь`,
         type: getRandomProperty(type),
         rooms: getRandomInteger(1, 100),
         guests: getRandomInteger(1, 100),
         checkin: checkinTime[getRandomInteger(0, checkinTime.length - 1)],
         checkout: checkoutTime[getRandomInteger(0, checkoutTime.length - 1)],
-        features: getRandomFeatures(someFeachure),
+        features: getRandomFeatures(getRandomInteger(0, allFeatures.length)),
         description: 'Описание',
-        photos: getRandomPhotos(somePhoto)
+        photos: getRandomPhotos(getRandomInteger(0, allPhotos.length))
       },
       location: {
         x: getRandomInteger(0, MAP_WIDTH),
@@ -82,29 +80,55 @@ function generateOffersArray(quantity) {
   return offers;
 }
 
+function generateFeachures(featuresList) {
+  const fragment = document.createDocumentFragment();
+  for (let i = 0; i < featuresList.length; i++) {
+    const featureItem = document.createElement('li');
+    featureItem.classList.add(`popup__feature`, `popup__feature--${featuresList[i]}`);
+    fragment.appendChild(featureItem);
+  }
+  return fragment;
+}
 
-/* function renderOffers(offer) {
+function generatePhotos(photosList) {
+  const fragment = document.createDocumentFragment();
+  for (let i = 0; i < photosList.length; i++) {
+    const photoItem = document.createElement('img');
+    photoItem.classList.add(`popup__photo`);
+    photoItem.width = 45;
+    photoItem.height = 40;
+    photoItem.src = photosList[i];
+    fragment.appendChild(photoItem);
+  }
+  return fragment;
+}
+
+
+function generateOffers(offer) {
   const offerElement = cardTemplate.cloneNode(true);
+  // Стирает удобства и фото из шаблона
+  offerElement.querySelector('.popup__features').innerHTML = '';
+  offerElement.querySelector('.popup__photos').innerHTML = '';
 
-  offerElement.querySelector('.popup__avatar').src = ad.author.avatar;
-  offerElement.querySelector('.popup__title').textContent = ad.offer.title;
-  offerElement.querySelector('.popup__text--address').textContent = ad.offer.adress;
-  offerElement.querySelector('.popup__text--price').textContent = ad.offer.prise;
-  offerElement.querySelector('.popup__type').textContent = ad.offer.type;
-  offerElement.querySelector('.popup__text--capacity').textContent = `${ad.offer.rooms} комнаты для ${ad.offer.guests} гостей`;
-  offerElement.querySelector('.popup__text--time').textContent = `Заезд после ${ad.offer.checkin}, выезд до ${ad.offer.checkout}`;
-  offerElement.querySelector('.popup__feature').textContent = ad.offer.feachures; // ???
-  offerElement.querySelector('.popup__description').textContent = ad.offer.description;
-  offerElement.querySelector('.popup__photo').src = ad.author.photos; // ???
-  return offerElement; // не поняла, нужно ли выводить на карту объявления
-} */
+  offerElement.querySelector('.popup__avatar').src = offer.author.avatar;
+  offerElement.querySelector('.popup__title').textContent = offer.offer.title;
+  offerElement.querySelector('.popup__text--address').textContent = offer.offer.adress;
+  offerElement.querySelector('.popup__text--price').textContent = offer.offer.price;
+  offerElement.querySelector('.popup__type').textContent = offer.offer.type;
+  offerElement.querySelector('.popup__text--capacity').textContent = `${offer.offer.rooms} комнаты для ${offer.offer.guests} гостей`;
+  offerElement.querySelector('.popup__text--time').textContent = `Заезд после ${offer.offer.checkin}, выезд до ${offer.offer.checkout}`;
+  offerElement.querySelector('.popup__features').appendChild(generateFeachures(offer.offer.features));
+  offerElement.querySelector('.popup__description').textContent = offer.offer.description;
+  offerElement.querySelector('.popup__photos').appendChild(generatePhotos(offer.offer.photos));
+  return offerElement;
+}
 
 function generatePin(pin) {
   const pinElement = pinTemplate.cloneNode(true);
   pinElement.querySelector('img').src = pin.author.avatar;
   pinElement.querySelector('img').alt = pin.offer.title;
-  pinElement.style.left = `${getRandomInteger(0, 1200) - PIN_WIDTH / 2}px`; // координаты по Х острого конца метки
-  pinElement.style.top = `${getRandomInteger(130, 630) - PIN_HEIGHT}px`; // координаты по Y острого конца метки
+  pinElement.style.left = `${getRandomInteger(0, 1200) - PIN_WIDTH / 2}px`;
+  pinElement.style.top = `${getRandomInteger(130, 630) - PIN_HEIGHT}px`;
   return pinElement;
 }
 
@@ -119,4 +143,17 @@ function showPins() {
   mapPins.appendChild(fragment);
 }
 
+
+function showOffers() {
+  map.classList.remove('map--faded');
+  const offers = generateOffersArray(8);
+  const fragment = document.createDocumentFragment();
+  for (let i = 0; i < offers.length; i++) {
+    fragment.appendChild(generateOffers(offers[i]));
+  }
+  map.insertBefore(fragment, mapFilter);
+}
+
+
 showPins();
+showOffers();
