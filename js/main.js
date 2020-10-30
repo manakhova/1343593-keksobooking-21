039cdebb06bad0df1123496d861bc076133738e6
Tatiana Manakhova <tat.manakhova@gmail.com>
2020-10-30 21:26:39 +0300

    Валидирует форму

--- js/main.js
+++ js/main.js
@@ -8,6 +8,12 @@ const mapPinMain = document.querySelector(`.map__pin--main`);
 const mapPins = map.querySelector(`.map__pins`);
 const mapFilter = map.querySelector(`.map__filters-container`);
 const addressInput = document.querySelector(`#address`);
+const capacity = document.querySelector(`#capacity`);
+const roomNumber = document.querySelector(`#room_number`);
+const capacityOptions = capacity.children;
+const titleInput = document.querySelector(`#title`);
+const priceInput = document.querySelector(`#price`);
+const typeSelect = document.querySelector(`#type`);
 const cardTemplate = document.querySelector(`#card`).content.querySelector(`.map__card`);
 cardTemplate.querySelector(`.popup__features`).innerHTML = ``;
 cardTemplate.querySelector(`.popup__photos`).innerHTML = ``;
@@ -30,6 +36,13 @@ const MAIN_PIN_TAIL_HEIGHT = 22;
 const MAP_WIDTH = 1200;
 const MAP_HEIGHT_TOP = 130;
 const MAP_HEIGHT_BOTTOM = 630;
+const MIN_TITLE_LENGTH = 30;
+const MAX_TITLE_LENGTH = 100;
+const MAX_PRICE = 1000000;
+const MIN_BUNGALOW_PRICE = 0;
+const MIN_FLAT_PRICE = 1000;
+const MIN_HOUSE_PRICE = 5000;
+const MIN_PALACE_PRICE = 10000;
 
 
 function getRandomInteger(min, max) {
@@ -45,12 +58,12 @@ function getRandomProperty(obj) {
 
 // Перемешивание массива по методу Фишера-Йейтса
 function shuffleArray(array) {
-  const copyArray = array.slice();
-  for (let i = array.length - 1; i > 0; i--) {
+  const items = array.slice();
+  for (let i = items.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
+    [items[i], items[j]] = [items[j], items[i]];
   }
-  return copyArray;
+  return items;
 }
 
 
@@ -162,7 +175,7 @@ function getMainPinAddress() {
 
 function mainPinMouseDown() {
   activatePage();
-} // это нормальное название для функции?
+}
 
 function mainPinKeyDown(evt) {
   if (evt.key === `Enter`) {
@@ -173,15 +186,19 @@ function mainPinKeyDown(evt) {
 function removeCards() {
   const currentCards = document.querySelectorAll(`article.map__card`);
   currentCards.forEach((card) => {
+    const cardButtonClose = card.querySelector(`button.popup__close`);
+    cardButtonClose.removeEventListener(`click`, closeCardClick);
+    cardButtonClose.removeEventListener(`keydown`, closeCardEscKeydown);
+
     card.remove();
   });
 }
 
-function closeCard() {
+function closeCardClick() {
   removeCards();
 }
 
-function closeCardEsc(evt) {
+function closeCardEscKeydown(evt) {
   if (evt.key === `Escape`) {
     removeCards();
   }
@@ -198,14 +215,104 @@ function mapClickFabric(offers) {
         const card = createCardElement(offers[offerId]);
         map.insertBefore(card, mapFilter);
 
-        const cardButtonClose = document.querySelector(`button.popup__close`);
-        cardButtonClose.addEventListener(`click`, closeCard);
-        cardButtonClose.addEventListener(`keydown`, closeCardEsc);
+        const cardButtonClose = card.querySelector(`button.popup__close`);
+        cardButtonClose.addEventListener(`click`, closeCardClick);
+        cardButtonClose.addEventListener(`keydown`, closeCardEscKeydown);
       }
     }
   };
 }
 
+// валидация формы
+function disabledCapacities() {
+  Array.from(capacityOptions).forEach((capacityOption) => {
+    capacityOption.setAttribute(`disabled`, `disabled`);
+  });
+}
+
+function validateCapacity() {
+  disabledCapacities();
+
+  if (roomNumber.value === `1`) {
+    Array.from(capacityOptions).forEach((capacityOption) => {
+      if (capacityOption.value === `1`) {
+        capacityOption.removeAttribute(`disabled`, `disabled`);
+      }
+    });
+  } else if (roomNumber.value === `2`) {
+    Array.from(capacityOptions).forEach((capacityOption) => {
+      if (capacityOption.value === `2` || capacityOption.value === `1`) {
+        capacityOption.removeAttribute(`disabled`, `disabled`);
+      }
+    });
+  } else if (roomNumber.value === `3`) {
+    Array.from(capacityOptions).forEach((capacityOption) => {
+      if (capacityOption.value !== `0`) {
+        capacityOption.removeAttribute(`disabled`, `disabled`);
+      }
+    });
+  } else {
+    Array.from(capacityOptions).forEach((capacityOption) => {
+      if (capacityOption.value === `0`) {
+        capacityOption.removeAttribute(`disabled`, `disabled`);
+      }
+    });
+  }
+}
+
+function validateType() {
+  if (typeSelect.value === `bungalow`) {
+    priceInput.min = MIN_BUNGALOW_PRICE;
+    priceInput.placeholder = `0`;
+    priceInput.setCustomValidity(`Минимальная цена за ночь - ${MIN_BUNGALOW_PRICE}`);
+  } else if (typeSelect.value === `flat`) {
+    priceInput.min = MIN_FLAT_PRICE;
+    priceInput.placeholder = `1000`;
+    priceInput.setCustomValidity(`Минимальная цена за ночь - ${MIN_FLAT_PRICE}`);
+  } else if (typeSelect.value === `house`) {
+    priceInput.min = MIN_HOUSE_PRICE;
+    priceInput.placeholder = `5000`;
+    priceInput.setCustomValidity(`Минимальная цена за ночь - ${MIN_HOUSE_PRICE}`);
+  } else if (typeSelect.value === `palace`) {
+    priceInput.min = MIN_PALACE_PRICE;
+    priceInput.placeholder = `10000`;
+    priceInput.setCustomValidity(`Минимальная цена за ночь - ${MIN_PALACE_PRICE}`);
+  } else {
+    priceInput.setCustomValidity(``);
+  }
+  priceInput.reportValidity();
+}
+
+function validateTitleInput() {
+  const valueLength = titleInput.value.length;
+
+  if (valueLength < MIN_TITLE_LENGTH) {
+    titleInput.setCustomValidity(`Еще ${(MIN_TITLE_LENGTH - valueLength)} симв.`);
+  } else if (valueLength > MAX_TITLE_LENGTH) {
+    titleInput.setCustomValidity(`Удалите лишние ${valueLength - MAX_TITLE_LENGTH} симв.`);
+  } else {
+    titleInput.setCustomValidity(``);
+  }
+
+  titleInput.reportValidity();
+}
+
+function validatePriceInput() {
+  if (priceInput.value > 1000000) {
+    priceInput.setCustomValidity(`Максимальная цена за ночь - ${MAX_PRICE}`);
+  } else {
+    priceInput.setCustomValidity(``);
+  }
+  priceInput.reportValidity();
+}
+
+const timeIn = document.querySelector(`#timein`);
+const timeOut = document.querySelector(`#timeout`);
+
+function synchronizeTime(evt) {
+  timeIn.value = evt.target.value;
+  timeOut.value = evt.target.value;
+}
 
 function activatePage() {
   map.classList.remove(`map--faded`);
@@ -221,9 +328,18 @@ function activatePage() {
   const offers = generateOffersArray(8);
   generatePins(offers);
 
+  validateCapacity();
+  validateType();// вызываю их тут, чтобы при активации страницы, валидация работала на выбранных по умолчанию вариантах
+
   mapPinMain.removeEventListener(`mousedown`, mainPinMouseDown);
   mapPinMain.removeEventListener(`keydown`, mainPinKeyDown);
+  roomNumber.addEventListener(`change`, validateCapacity);
   map.addEventListener(`click`, mapClickFabric(offers));
+  titleInput.addEventListener(`input`, validateTitleInput);
+  priceInput.addEventListener(`input`, validatePriceInput);
+  typeSelect.addEventListener(`change`, validateType);
+  timeIn.addEventListener(`change`, synchronizeTime);
+  timeOut.addEventListener(`change`, synchronizeTime);
 }
 
 
