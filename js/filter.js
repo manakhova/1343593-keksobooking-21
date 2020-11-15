05b57478067ebfdbd95b577b4ec28a687c596ed8
Tatiana Manakhova <tat.manakhova@gmail.com>
2020-11-15 23:07:18 +0300

    Доделывает фильтрацию

--- js/filter.js
+++ js/filter.js
@@ -1,20 +1,54 @@
 'use strict';
 
 (function () {
+  // const {debounce} = window.utils;
   const {removeCards, removePins, renderPins} = window.map;
   const {generatePins} = window.pin;
   const housingType = document.querySelector(`#housing-type`);
+  const housingPrice = document.querySelector(`#housing-price`);
+  const housingRooms = document.querySelector(`#housing-rooms`);
+  const housingGuests = document.querySelector(`#housing-guests`);
+  const housingFeatures = document.querySelector(`#housing-features`);
   let filteredOffers = [];
   let data = [];
+  const LOW = 10000;
+  const HIGH = 50000;
+  // const DEBOUNCE_INTERVAL = 500;
 
 
   function filterByType(item) {
     return housingType.value === `any` || housingType.value === item.offer.type ? true : false;
   }
 
+  function filterByPrice(item) {
+    if (housingPrice.value === `any` ||
+    (housingPrice.value === `low` && item.offer.price < LOW) ||
+    (housingPrice.value === `middle` && item.offer.price >= LOW && item.offer.price < HIGH) ||
+    (housingPrice.value === `high` && item.offer.price >= HIGH)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function filterByRooms(item) {
+    return housingRooms.value === `any` || housingRooms.value === item.offer.rooms.toString() ? true : false;
+  }
+
+  function filterByGuests(item) {
+    return housingGuests.value === `any` || housingGuests.value === item.offer.guests.toString() ? true : false;
+  }
+
+  function filterByFeatures(item) {
+    const checkedFeatures = housingFeatures.querySelectorAll(`input:checked`);
+    return Array.from(checkedFeatures).every((feature) => {
+      return item.offer.features.includes(feature.value) ? true : false;
+    });
+  }
+
   function onFilterChange() {
     filteredOffers = data;
-    filteredOffers = filteredOffers.filter(filterByType);
+    filteredOffers = filteredOffers.filter(filterByType).filter(filterByPrice).filter(filterByRooms).filter(filterByGuests).filter(filterByFeatures);
 
     removePins();
     removeCards();
@@ -25,6 +59,10 @@
   function activateFilter(offers) {
     data = offers;
     housingType.addEventListener(`change`, onFilterChange);
+    housingPrice.addEventListener(`change`, onFilterChange);
+    housingRooms.addEventListener(`change`, onFilterChange);
+    housingGuests.addEventListener(`change`, onFilterChange);
+    housingFeatures.addEventListener(`change`, onFilterChange);
   }
 
   window.filter = {
--- js/utils.js
+++ js/utils.js
@@ -19,9 +19,20 @@
     return items;
   }
 
+  /* function debounce(cb, timeout) {
+    let timer;
+    return function () {
+      if (timer) {
+        clearTimeout(timer);
+      }
+      timer = setTimeout(cb, timeout);
+    };
+  } */
+
   window.utils = {
     getRandomInteger,
     getRandomProperty,
-    shuffleArray
+    shuffleArray,
+    // debounce
   };
 })();
